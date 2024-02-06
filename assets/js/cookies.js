d02190c475c9930ac78360c22f2fd3ec14ccaf47
Cyntia Sayuri <cy.sayuri@hotmail.com>
2024-02-06 17:21:54 -0300

    Atualizações e ajustes

--- assets/js/cookies.js
+++
@@ -1,6 +0,0 @@
-// const cookies = document.querySelector('.cookies')
-// const closeCookies = document.querySelector('.cookies__close')
-
-// closeCookies.addEventListener('click', () => {
-//     cookies.classList.add('disable')
-// })
